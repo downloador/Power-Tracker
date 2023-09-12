2cece6911bc7ae5c0c6c6026b4348fd3dcbcc732
downloador <38085070+downloador@users.noreply.github.com>
2023-09-12 18:42:02 +0200

    wip

--- Hardware.cs
+++ Hardware.cs
@@ -6,28 +6,32 @@ namespace Power_Tracker
 {
     public class Hardware
     {
-        public static double CPU_TotalPowerDraw = 0;
-        private static List<double> CPU_AverageTemperature = new List<double>();
-        private static int TimeAverage = 15;
+        public int UpdateRateInMs = 100;
+        public int TimeAverage = 15000;
+        public int ListSize = TimeAverage / UpdateRateInMs;
+
+        public List<double> CPU_AverageTempList = new List<double>();
+        //
+
+        public List<double> CPU_AverageDrawList = new List<double>();
+        public double CPU_TotalDraw = 0;
+        // Watt is a measurement in hour, check how high it is, how long since last check which
+        // tells us how many watts it used in that millisecond timespan
         
-        public static double Epoch_ProgramStart; // Assignee by Controller in Main
-        public static double Epoch_FileLastWritten;
-        private static double Epoch_LastCheck;
+        public double Epoch_ProgramStart; // Assignee by Controller in Main
+        public double Epoch_FileLastWritten;
+        public double Epoch_LastCheck;
 
-        public static bool AllowedToWrite = false;
+        public bool AllowedToWrite = false;
 
-        private static Computer LocalComputer = new Computer()
+        private Computer LocalComputer = new Computer()
         {
             CPUEnabled = true,
         };
 
-        public static double[] GetProcessorInfo()
+        public void UpdateInfo() 
         {
             TimeSpan CurrentTime = Controller.RetrieveCurrentTime();
-
-            double CPU_PowerDraw = 0;
-            double CPU_Temperature = 0;
-            double[] returnData = new double[3];
 
             foreach (var hardware in LocalComputer.Hardware)
             {
@@ -38,45 +42,58 @@ namespace Power_Tracker
                         foreach (var sensor in hardware.Sensors)
                             if (sensor.SensorType == SensorType.Power && sensor.Name.Contains("CPU Package"))
                             {
-                                CPU_PowerDraw = Math.Round(sensor.Value.GetValueOrDefault());
-                                CPU_TotalPowerDraw += (CPU_PowerDraw / 3600000) * (CurrentTime.TotalMilliseconds - Epoch_LastCheck);
+                                double Scope_CPU_Draw = Math.Round(sensor.Value.GetValueOrDefault());
+                                double ActiveDrawPerMs = Scope_CPU_Draw / (60 * 60 * 1000);
+                                double DurationInMs = CurrentTime.TotalMilliseconds - Epoch_LastCheck;
 
-                                using (StreamWriter outputFile = new StreamWriter(Application.StartupPath + "\\Wattage.txt"))
-                                {
-                                    string OutputData = Convert.ToString(CPU_TotalPowerDraw);
-                                    OutputData = OutputData.Replace(".", ",");
+                                Console.WriteLine(DurationInMs);
 
-                                    outputFile.WriteLine("Wattage : " + OutputData);
-                                    outputFile.Close();					
-                                }
+                                CPU_TotalDraw += Scope_CPU_Draw / ActiveDrawPerMs * DurationInMs
+                                // Draw divided by 3,6m ms * since last check to get total draw
+
+                                if (CPU_AverageDrawList.Count >= ListSize) CPU.CPU_AverageDrawList.RemoveAt(0);
+                                CPU_AverageDrawList.Add(Scope_CPU_Draw);
                             }
                             else if (sensor.SensorType == SensorType.Temperature && sensor.Name.Contains("CPU Package"))
                             {
-                                CPU_Temperature = sensor.Value.GetValueOrDefault();
+                                double Scope_CPU_Temperature = sensor.Value.GetValueOrDefault();
 
-                                if (CPU_AverageTemperature.Count >= TimeAverage) CPU_AverageTemperature.RemoveAt(0);
-                                CPU_AverageTemperature.Add(CPU_Temperature);
+                                if (CPU_AverageTempList.Count >= ListSize) CPU_AverageTempList.RemoveAt(0);
+                                CPU_AverageTempList.Add(Scope_CPU_Temperature);
                             }
                     }
                 }
             }
 
             Epoch_LastCheck = CurrentTime.TotalMilliseconds;
-
-            returnData[0] = CPU_PowerDraw;
-            returnData[1] = Math.Round(CPU_TotalPowerDraw/1000, 2);
-            returnData[2] = CPU_AverageTemperature.Count > 0 ? Math.Round(CPU_AverageTemperature.Average(), 1) : 0.0;
-
-            if (AllowedToWrite == true) Console.Write("\r[" + new DateTime(CurrentTime.Ticks).ToString("HH:mm:ss") + "]    " + $"Power: {CPU_PowerDraw}w    Total Power: {returnData[1]}kWh    Temperature: {returnData[2]}°C               ");
-
-            return returnData;
         }
 
-        public static void Initialize()
+        public double[] RetrieveInfo()
         {
-            LocalComputer.Open();
+            double[] ReturnData = new double[3];
+
+            ReturnData[0] = CPU_AverageDrawList.Count > 0 ? Math.Round(CPU_AverageDrawList.Average(), 1) : 0.0;
+            ReturnData[1] = Math.Round(CPU_TotalPowerDraw/1000, 2); // in kWh
+            ReturnData[2] = CPU_AverageTemperature.Count > 0 ? Math.Round(CPU_AverageTemperature.Average(), 1) : 0.0;
+
+            return ReturnData;
+        }
+
+        public void Start()
+        {
+            LocalComputer.Open()
+
+            Thread.Sleep(5000);
+            // wait a while just incase (i havent tried)
 
             Epoch_LastCheck = Epoch_ProgramStart = Controller.RetrieveCurrentTime().TotalMilliseconds;
+
+            while (true)
+            {
+                UpdateInfo()
+
+                Thread.Sleep(UpdateRateInMs);
+            }
         }
     }
 }
